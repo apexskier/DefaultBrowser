7b08ef29e982d22f3995d869eb8322cf3634e05b
Cameron Little <cameron@camlittle.com>
2015-10-23 16:14:52 -0700

    Initial Commit

---
+++ DefaultBrowser/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DefaultBrowser
+//
+//  Created by Cameron Little on 10/23/15.
+//  Copyright © 2015 Cameron Little. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
