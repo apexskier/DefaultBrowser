d8ad84c27d329029bba261f247c25911505c2e35
Cameron Little <cameron@camlittle.com>
2019-09-29 17:38:26 +0200

    fix and update signing

--- DefaultBrowserTests/DefaultBrowserTests.swift
+++ DefaultBrowserTests/DefaultBrowserTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import DefaultBrowser
 
 class DefaultBrowserTests: XCTestCase {
     
@@ -28,7 +27,7 @@ class DefaultBrowserTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
